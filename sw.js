48f6bfe29b5149a2fc01e15a737b256c7a03c374
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-11-21 09:16:06 +0000

    Deploy mazipan/www-nontech-summit-2019 to github.com/mazipan/www-nontech-summit-2019.git:gh-pages

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.36dc4490ca2a5d04176ce49fe120db95.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("/precache-manifest.1283ee7d064053ad77c9805d8f36203f.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 workbox.setConfig({
   debug: false
