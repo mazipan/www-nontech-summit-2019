abdf2004eb2e8c6429930b527af470f6871f60cd
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-11-27 09:49:55 +0000

    Deploy mazipan/www-nontech-summit-2019 to github.com/mazipan/www-nontech-summit-2019.git:gh-pages

--- precache-manifest.4a779270bd222609b57d46366c9f8fea.js
+++ precache-manifest.4a779270bd222609b57d46366c9f8fea.js
@@ -1,46 +1,18 @@
 self.__precacheManifest = [
   {
-    "revision": "1618db5a90c3947e1bf37e94b3b057c5",
-    "url": "/images/favicon/apple-icon-57x57.png"
-  },
-  {
-    "revision": "335f84b4c528b84fb27ed0a623d3b0b3",
-    "url": "/images/banner/building.jpg"
-  },
-  {
-    "url": "/vendors~app.f6a5194399fed3bcff82.js"
-  },
-  {
-    "revision": "6cc9f1b836ffad124f0baedc5036669f",
-    "url": "/index.html"
-  },
-  {
-    "revision": "1ad660bd55e88e8fbdd064034c04584e",
-    "url": "/images/venue/ayana_ballroom_1.jpg"
-  },
-  {
-    "revision": "f85f301296b9fea59402b882fbeb5565",
-    "url": "/images/venue/ayana_ballroom_2.jpg"
+    "revision": "2cfce1a7044325708db262168dcc90f1",
+    "url": "/images/favicon/apple-icon-152x152.png"
   },
   {
     "revision": "cc401143f5003c958a917711f57908f8",
     "url": "/images/venue/ayana_ballroom_3.jpg"
   },
   {
-    "revision": "63ac6459d8a4e2410772ac65addea2b2",
-    "url": "/register/index.html"
+    "url": "/vendors~app.0343d62b723f0a9d7fe9.js"
   },
   {
-    "revision": "aa74061cd2efc5e079835a23e96b588a",
-    "url": "/speaker/index.html"
-  },
-  {
-    "revision": "d6507c735cca336d1c91172a7869e64c",
-    "url": "/sponsor/index.html"
-  },
-  {
-    "revision": "ad824fa0214bcc6326624dba554990df",
-    "url": "/volunteer/index.html"
+    "revision": "adbba4a76ddadf8c8dc29289012e927b",
+    "url": "/index.html"
   },
   {
     "revision": "4f15d69470e5003817d7e3f07923bae2",
@@ -51,43 +23,79 @@ self.__precacheManifest = [
     "url": "/images/venue/ayana_ballroom_3_300px.jpg"
   },
   {
-    "url": "/vendors~app.f6a5194399fed3bcff82.js.gz"
-  },
-  {
-    "revision": "bb85dfc09398d495580604f47cb8dc89",
-    "url": "/index.html.gz"
-  },
-  {
     "revision": "db4f8ce6d5673df7fcdbc26c0f38c8fc",
     "url": "/images/venue/ayana_ballroom_3_300px.jpeg"
   },
   {
-    "revision": "db6934fe060db41254a65c947ed9aacc",
-    "url": "/images/banner/building.webp"
+    "revision": "fdf75259e11e43204d8477e7419d81bf",
+    "url": "/register/index.html"
+  },
+  {
+    "revision": "d647253dd4f1f52e0942da01caa90b9e",
+    "url": "/speaker/index.html"
+  },
+  {
+    "revision": "af462559cbe1df3aff1d887f98399aa6",
+    "url": "/sponsor/index.html"
+  },
+  {
+    "revision": "1671e9b0192159fc61af65aafacf0b22",
+    "url": "/volunteer/index.html"
+  },
+  {
+    "revision": "ba7c369d7bf081b3052a9cfa92f6ff22",
+    "url": "/index.html.gz"
+  },
+  {
+    "revision": "f897b1970d321d9f0d0b14c22ac16fcc",
+    "url": "/volunteer/index.html.gz"
+  },
+  {
+    "url": "/vendors~app.0343d62b723f0a9d7fe9.js.gz"
+  },
+  {
+    "revision": "91ac9bbbdb1d0f900d3535c226bf8a4f",
+    "url": "/sponsor/index.html.gz"
+  },
+  {
+    "revision": "82da2e2abb83e5488b123890a95fc46f",
+    "url": "/speaker/index.html.gz"
+  },
+  {
+    "revision": "f3cc9011d51f8204c911f46290ad9e13",
+    "url": "/register/index.html.gz"
+  },
+  {
+    "revision": "c05b62ef352b868cf2db1d5c8b04e5d3",
+    "url": "/images/favicon/favicon-16x16.png"
+  },
+  {
+    "revision": "f5a307b0392302e1f5cd3d1602535f2a",
+    "url": "/faq/index.html.gz"
+  },
+  {
+    "revision": "f85f301296b9fea59402b882fbeb5565",
+    "url": "/images/venue/ayana_ballroom_2.jpg"
   },
   {
     "revision": "9d6af22d7a611c3da00115e060017c6c",
     "url": "/images/venue/ayana_ballroom_2_300px.jpeg"
   },
   {
-    "revision": "1dcb647b268f4e303b5ae712b1e59b5c",
-    "url": "/register/index.html.gz"
+    "revision": "1ad660bd55e88e8fbdd064034c04584e",
+    "url": "/images/venue/ayana_ballroom_1.jpg"
   },
   {
-    "revision": "372f69623be5712d06a6b758c4424d48",
-    "url": "/speaker/index.html.gz"
+    "revision": "0884edd790388861f7cc507542f52ace",
+    "url": "/images/banner/building.jpeg"
   },
   {
-    "revision": "1b13b2c9d0cf077da650552d0c570945",
-    "url": "/sponsor/index.html.gz"
+    "revision": "335f84b4c528b84fb27ed0a623d3b0b3",
+    "url": "/images/banner/building.jpg"
   },
   {
-    "revision": "376509d8eab8052f7036011a9524a453",
-    "url": "/volunteer/index.html.gz"
-  },
-  {
-    "revision": "863a25d7c3f51f802f9a63cb3d967931",
-    "url": "/images/venue/ayana_ballroom_1_300px.jpeg"
+    "revision": "db6934fe060db41254a65c947ed9aacc",
+    "url": "/images/banner/building.webp"
   },
   {
     "revision": "ffd3778235bebf085eaf77e3c46aec2e",
@@ -130,16 +138,16 @@ self.__precacheManifest = [
     "url": "/images/favicon/apple-icon-144x144.png"
   },
   {
-    "revision": "2cfce1a7044325708db262168dcc90f1",
-    "url": "/images/favicon/apple-icon-152x152.png"
+    "revision": "863a25d7c3f51f802f9a63cb3d967931",
+    "url": "/images/venue/ayana_ballroom_1_300px.jpeg"
   },
   {
     "revision": "b94b865d4a58a4dc7f0a6986f3d690f0",
     "url": "/images/favicon/apple-icon-180x180.png"
   },
   {
-    "revision": "7a7144ac23c07476604a9d73e01d7b57",
-    "url": "/images/sponsor/biznet-gio-logo.webp"
+    "revision": "1618db5a90c3947e1bf37e94b3b057c5",
+    "url": "/images/favicon/apple-icon-57x57.png"
   },
   {
     "revision": "798dc1f5c71186098a321a6c26da4433",
@@ -162,8 +170,8 @@ self.__precacheManifest = [
     "url": "/images/favicon/apple-icon.png"
   },
   {
-    "revision": "c05b62ef352b868cf2db1d5c8b04e5d3",
-    "url": "/images/favicon/favicon-16x16.png"
+    "revision": "7a7144ac23c07476604a9d73e01d7b57",
+    "url": "/images/sponsor/biznet-gio-logo.webp"
   },
   {
     "revision": "c188d3145fcdd90b289eb196f073ef39",
@@ -234,43 +242,35 @@ self.__precacheManifest = [
     "url": "/images/sponsor/biznet-gio-logo.svg"
   },
   {
-    "url": "/app.f6a5194399fed3bcff82.js.gz"
-  },
-  {
-    "url": "/app.f6a5194399fed3bcff82.js"
-  },
-  {
-    "revision": "bee1535f97890a298ddc02bd031f7370",
-    "url": "/faq/index.html.gz"
-  },
-  {
-    "revision": "b32d061dc0037120d3dfad118f7b9e2c",
-    "url": "/contact/index.html.gz"
-  },
-  {
-    "revision": "372f69623be5712d06a6b758c4424d48",
-    "url": "/coacher/index.html.gz"
-  },
-  {
-    "revision": "0884edd790388861f7cc507542f52ace",
-    "url": "/images/banner/building.jpeg"
-  },
-  {
-    "revision": "aa74061cd2efc5e079835a23e96b588a",
-    "url": "/coacher/index.html"
-  },
-  {
-    "revision": "ff2d580a643fcbd2d507c357c55a7f7a",
+    "revision": "741995047c4010205c10617db7ab27ea",
     "url": "/faq/index.html"
   },
   {
-    "revision": "548f5c962fad0cf23cb4c23fd73a3a3a",
+    "revision": "a4af5010d348dcd53de952e731c2a99f",
+    "url": "/contact/index.html.gz"
+  },
+  {
+    "revision": "26758e667329f5c61eb1295ee2029676",
     "url": "/contact/index.html"
   },
   {
-    "url": "/app.f6a5194399fed3bcff82.css.gz"
+    "revision": "82da2e2abb83e5488b123890a95fc46f",
+    "url": "/coacher/index.html.gz"
   },
   {
-    "url": "/app.f6a5194399fed3bcff82.css"
+    "revision": "d647253dd4f1f52e0942da01caa90b9e",
+    "url": "/coacher/index.html"
+  },
+  {
+    "url": "/app.0343d62b723f0a9d7fe9.js.gz"
+  },
+  {
+    "url": "/app.0343d62b723f0a9d7fe9.js"
+  },
+  {
+    "url": "/app.0343d62b723f0a9d7fe9.css.gz"
+  },
+  {
+    "url": "/app.0343d62b723f0a9d7fe9.css"
   }
 ];
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.bfdc514d13f890d2d0917e5b90e18bdc.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("/precache-manifest.4a779270bd222609b57d46366c9f8fea.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 /* eslint-disable */
 workbox.setConfig({
